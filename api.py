a9fcf30407ae01e467d2fe97254f628734e1fa09
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2021-04-18 18:24:00 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from flask import Flask,jsonify,json
-import pandas as pd
-import numpy as np
+from flask import Flask, jsonify, url_for, redirect
+import pandas as pd, numpy as np
 from build_model import load_model
 
 system=np.load("utils/cosine_similarity.npy")
@@ -10,41 +9,42 @@ indices=pd.read_csv("utils/indices.csv")["_id"]
 
 
 def recommend(_id, cosine_sim = system):
-    recommended_texts = []
     idx = indices[indices == _id].index[0]
-    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+    score_series = pd.Series(
+        cosine_sim[idx]
+    ).sort_values(ascending = False)
     top_2_indices = list(score_series.iloc[1:3].index)
 
-
-    for i in top_2_indices:
-        recommended_texts.append(list(indices)[i])
-
-    return recommended_texts
+    return [list(indices)[i] for i in top_2_indices]
 
 
 
 def get_recommendations(_id):
     recommended_texts_id=recommend(_id)
-    recommendations_list_of_dict=[]
-    dict_first={"0":recommended_texts_id[0]}
-    dict_second={"1":recommended_texts_id[1]}
-    recommendations_list_of_dict.append(dict_first)
-    recommendations_list_of_dict.append(dict_second)
-    return recommendations_list_of_dict
-
-
-
+    return [
+        {"0": recommended_texts_id[0]},
+        {"1": recommended_texts_id[1]}
+    ]
 
 
 app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect('https://documenter.getpostman.com/view/11853513/TzJsfJSw')
 
 @app.route("/recommendations/<_id>",methods=["GET"])
 def api(_id):
     try:
         recommendations=get_recommendations(_id)
-        return jsonify({"data":recommendations,"message":f"recommendations successfully gotten for id:{_id}"}),200
+        return jsonify({
+            "data":recommendations,
+            "message":f"recommendations successfully gotten for id:{_id}"
+        }),200
     except :
-        return jsonify({"message":f"recommendations couldn't be gotten for id:{_id}"}),404
+        return jsonify({
+            "message":f"recommendations couldn't be gotten for id:{_id}"
+        }),404
 
 @app.route("/update_recommendations")
 def update_system():
@@ -52,5 +52,4 @@ def update_system():
     return "",204
 
 if __name__ == '__main__':
-    app.run(debug=False)
-
+    app.run()
