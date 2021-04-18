43498ae45ddebc702f34c80179af8700975454d8
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-04-18 18:55:17 +0100

    Updated the response data

--- api.py
+++ api.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from flask import Flask, jsonify, url_for, redirect
-import pandas as pd, numpy as np
+from flask import Flask, jsonify, redirect
+from flask.json import load
+import pandas as pd, numpy as np, os
 from build_model import load_model
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+client = MongoClient(os.getenv('MONGODB_CONNECTION_STRING'))
+
+load_dotenv()
 
 system=np.load("utils/cosine_similarity.npy")
 indices=pd.read_csv("utils/indices.csv")["_id"]
@@ -22,10 +29,19 @@ def recommend(_id, cosine_sim = system):
 def get_recommendations(_id):
     recommended_texts_id=recommend(_id)
     return [
-        {"0": recommended_texts_id[0]},
-        {"1": recommended_texts_id[1]}
+        recommended_texts_id[0],
+        recommended_texts_id[1]
     ]
 
+def get_col_from_id(_id):
+    try:
+        from bson.objectid import ObjectId
+        data =  dict(client.upspace.data.find_one({'_id': ObjectId(_id)}))
+        data['_id'] = str(data['_id'])
+        return data
+    except Exception as e: 
+        print(str(e))
+        return
 
 app=Flask(__name__)
 
@@ -36,17 +52,25 @@ def home():
 @app.route("/recommendations/<_id>",methods=["GET"])
 def api(_id):
     try:
-        recommendations=get_recommendations(_id)
+        publication_data = get_col_from_id(_id)
+        if not publication_data:
+            return {
+                "message": "No publication found for that ID"
+            }, 404
+        publication_data['recommendations'] = [ 
+           get_col_from_id(recommended_id) 
+               for recommended_id in get_recommendations(_id)
+        ]
         return jsonify({
-            "data":recommendations,
-            "message":f"recommendations successfully gotten for id:{_id}"
+            "data":publication_data,
+            "message":f"recommendations successfully gotten for id"
         }),200
-    except :
+    except Exception as e :
         return jsonify({
             "message":f"recommendations couldn't be gotten for id:{_id}"
-        }),404
+        }),500
 
-@app.route("/update_recommendations")
+@app.route("/update_recommendations", methods=['POST'])
 def update_system():
     load_model()
     return "",204
